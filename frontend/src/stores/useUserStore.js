b32ba412238ef5966380f254329348405c70aa1a
Joab Almeida <joabalmeida.dev@gmail.com>
2025-07-29 20:39:08 -0300

    home page finished

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -51,7 +51,7 @@ export const userUserStore = create((set, get) => ({
     set({ checkingAuth: true });
     try {
       const response = await axios.get("/auth/profile");
-      set({ user: response.data, checkingAuth: false }); //we stop checking bc not loading anymore
+      set({ user: response.data, checkingAuth: false });
     } catch (error) {
       console.log(error.message);
       set({ checkingAuth: false, user: null });
