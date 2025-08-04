77dcdf6e2ba581b54059ac03dcb903715f2b983c
Joab Almeida <joabalmeida.dev@gmail.com>
2025-08-04 17:09:07 -0300

    first version completed

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 import cartRoutes from "./routes/cart.routes.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.routes.js";
@@ -13,6 +14,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); //allows to parse body from frontend
 app.use(cookieParser()); //allows to parse refreshToken from cookie
@@ -29,6 +32,13 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
 });
